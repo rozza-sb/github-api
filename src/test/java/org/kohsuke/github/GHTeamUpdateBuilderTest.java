7f3d758a43c1e7c0b6184ddfa349ecb33b9a9427
Rory Kelly <rory.kelly@starlingbank.com>
2026-04-13 13:12:15 +0100

    Update test comments.

--- src/test/java/org/kohsuke/github/GHTeamUpdateBuilderTest.java
+++ src/test/java/org/kohsuke/github/GHTeamUpdateBuilderTest.java
@@ -33,17 +33,18 @@ public class GHTeamUpdateBuilderTest extends AbstractGitHubWireMockTest {
     public GHTeamUpdateBuilderTest() {
     }
 
-    // update name, description, privacy, notifications, permission, no change to parent team
-    // update parent team
-    // update removal of parent team
-
+    /**
+     * Given a team, when updating the team with a different parent team, then the team is updated with the new parent team.
+     * @throws IOException exception thrown if there is an issue with Wiremock
+     */
     @Test
     public void testUpdateTeamWithNewParentTeam() throws IOException {
-        // Get the parent team
+        // Get the org and teams
         GHOrganization org = gitHub.getOrganization(GITHUB_API_TEST_ORG);
         GHTeam teamToUpdate = org.getTeamBySlug(TEAM_TO_UPDATE_SLUG);
         GHTeam newParentTeam = org.getTeamBySlug(NEW_PARENT_TEAM_SLUG);
 
+        // Update team with different parent team
         GHTeam updatedTeam = getCommonBuilder(teamToUpdate)
                 .parentTeamId(newParentTeam.getId())
                 .update();
@@ -52,24 +53,35 @@ public class GHTeamUpdateBuilderTest extends AbstractGitHubWireMockTest {
         // assertThat(updatedTeam.getParentTeam().getId(), equalTo(newParentTeam.getId()));
     }
 
+    /**
+     * Given a team, when updating the team with no change to parent team, then the team is updated with no change to the parent team.
+     * @throws IOException exception thrown if there is an issue with Wiremock
+     */
     @Test
     public void testUpdateTeamWithNoChangeToParentTeam() throws IOException {
-        // Get the parent team
+        // Get the org and team
         GHOrganization org = gitHub.getOrganization(GITHUB_API_TEST_ORG);
         GHTeam teamToUpdate = org.getTeamBySlug(TEAM_TO_UPDATE_SLUG);
         // GHTeam existingParentTeam = org.getTeamBySlug(CURRENT_PARENT_TEAM_SLUG);
 
+        // update team with no change to parent team
         GHTeam updatedTeam = getCommonBuilder(teamToUpdate).update();
 
         assertUpdatedTeam(updatedTeam);
         // assertThat(teamToUpdate.getParentTeam().getId(), equalTo(existingParentTeam.getId()));
     }
 
+    /**
+     * Given a team, when updating the team with a removed parent team, then the team is updated and has no parent team.
+     * @throws IOException exception thrown if there is an issue with Wiremock
+     */
     @Test
     public void testUpdateTeamWithRemovedParentTeam() throws IOException {
+        // Get the org and team
         GHOrganization org = gitHub.getOrganization(GITHUB_API_TEST_ORG);
         GHTeam teamToUpdate = org.getTeamBySlug(TEAM_TO_UPDATE_SLUG);
 
+        // Update team with removed parent team
         GHTeam updatedTeam = getCommonBuilder(teamToUpdate)
                 .parentTeamId(null)
                 .update();
@@ -78,6 +90,12 @@ public class GHTeamUpdateBuilderTest extends AbstractGitHubWireMockTest {
         // assertThat(teamToUpdate.getParentTeam(), equalTo(null));
     }
 
+    /**
+     * Get the GHTeamUpdateBuilder instance with the common fields set for updating a team, to be used in the different update scenarios.
+     *
+     * @param teamToUpdate the base team to update
+     * @return the GHTeamUpdateBuilder instance with the common fields set for updating a team
+     */
     private GHTeamUpdateBuilder getCommonBuilder(GHTeam teamToUpdate) {
         return teamToUpdate.updateTeam()
                 .name(TEAM_TO_UPDATE_NEW_NAME)
@@ -87,6 +105,11 @@ public class GHTeamUpdateBuilderTest extends AbstractGitHubWireMockTest {
                 .permission(TEAM_TO_UPDATE_NEW_PERMISSIONS);
     }
 
+    /**
+     * Assert that the updated team has the expected updated values.
+     *
+     * @param updatedTeam the team to assert the updated values on
+     */
     private void assertUpdatedTeam(GHTeam updatedTeam) {
         assertThat(updatedTeam.getName(), equalTo(TEAM_TO_UPDATE_NEW_NAME));
         assertThat(updatedTeam.getDescription(), equalTo(TEAM_TO_UPDATE_NEW_DESCRIPTION));
